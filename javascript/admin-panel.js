281a84fe44ab43931990eb790ad1e80eb18d52f9
ROUSHAN KUMAR <52878060+theRoushan@users.noreply.github.com>
2020-05-05 17:47:41 +0530

    updating admin panel

---
+++ javascript/admin-panel.js
@@ -0,0 +1,422 @@
+//Listening Events is to be handled here
+$('ul.dashboard-links li a[href="#"]').click(function(event) {
+    event.preventDefault();
+});
+$('ul.dashboard-links li').click(function(event) {
+    event.preventDefault();
+});
+
+//Function to get Server Time and Data
+function srvTime() {
+    try {
+        //FF, Opera, Safari, Chrome
+        xmlHttp = new XMLHttpRequest();
+    } catch (err1) {
+        //IE
+        try {
+            xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+        } catch (err2) {
+            try {
+                xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+            } catch (eerr3) {
+                //AJAX not supported, use CPU time.
+                alert("AJAX not supported");
+            }
+        }
+    }
+    xmlHttp.open('HEAD', window.location.href.toString(), false);
+    xmlHttp.setRequestHeader("Content-Type", "text/html");
+    xmlHttp.send('');
+    return xmlHttp.getResponseHeader("Date");
+}
+
+//var st = srvTime();
+//var date = new Date(st);
+
+function display_c() {
+    var refresh = 1000; // Refresh rate in milli seconds
+    mytime = setTimeout('display_ct()', refresh)
+}
+
+function display_ct() {
+    var st = srvTime();
+    var x = new Date(st);
+    var d = x + "";
+    var date = d.slice(0, 15);
+    var trainTime = d.slice(15, 25);
+    var tmp = parseInt(trainTime.slice(1, 3));
+    var hr, pre;
+    if (tmp >= 12) {
+        hr = tmp - 12;
+        pre = "PM";
+    } else if (tmp == 0) {
+        hr = 12;
+        pre = "AM";
+    } else {
+        hr = tmp;
+        pre = "AM";
+    }
+
+    var curr_time = hr + trainTime.slice(3, 9) + ' ' + pre;
+    var day = date.slice(8, 10);
+    var month = date.slice(4, 7);
+    var year = date.slice(11, 15);
+    var curr_date = day + ' ' + month + ', ' + year;
+    //new Date()
+    document.getElementById('time').innerHTML = curr_time;
+    document.getElementById('date').innerHTML = curr_date;
+    tt = display_c();
+}
+
+//Function to hide the nav sidebar
+function hamburger() {
+    var sidebar = document.getElementById("sidebar");
+    var main = document.getElementById("main");
+    sidebar.classList.toggle('hide-sidebar');
+    main.classList.toggle('full-container');
+}
+
+//hides the panel designed for not logged users and unhide the panel for logged user
+function showLoggedUserAccess() {
+    var elements = document.getElementsByClassName('logged-user');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.display = "flex";
+    }
+    var notlogged = document.getElementsByClassName("not-logged-user");
+    notlogged[0].style.display = "none";
+    document.getElementById('displayName').innerText = Auth.currentUser.displayName;
+}
+
+//hides the panel designed for logged users and unhide the panel for not logged user
+function hideLoggedUserAccess() {
+    var elements = document.getElementsByClassName('logged-user');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.display = "none";
+    }
+    var notlogged = document.getElementsByClassName("not-logged-user");
+    notlogged[0].style.display = "flex";
+}
+
+//this function is designed for logging in admin in the web app
+function adminLogin() {
+    var email = document.getElementById('login-email').value;
+    var pass = document.getElementById('login-pass').value;
+    const isAdmin = false;
+    Auth.signInWithEmailAndPassword(email, pass).then(function(user) {
+        console.log(user);
+        user.getIdTokenResult().then(idTokenResult => {
+            isAdmin = idTokenResult.claims.admin;
+        });
+        if (user && isAdmin) {
+            document.getElementById('login-panel').style.display = "none";
+            console.log("you are logged as an admin");
+        } else {
+            logout();
+            alert("you are not admin");
+        }
+    })
+}
+//this function is to get the data of user which is going to be admin
+const adminLoginBtn = document.getElementById('form-login-btn');
+adminLoginBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    adminLogin();
+})
+
+//Listens Click for VerifyAdmin Buttom
+var verifyAdmin = document.querySelector('#verify-admin');
+verifyAdmin.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.getElementById('updateRole').classList.toggle('show');
+    document.querySelector('#updateRole #purposeOfRole').innerText = "Verify Account Before Making Admin";
+    var elements = document.getElementsByClassName('input member');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.display = "none";
+    }
+    document.getElementById('make-member').style.display = "none";
+    document.getElementById('make-admin').style.display = "flex";
+    const adminEmail = document.getElementById('make-email-admin').value;
+    var docRef = db.collection('Users').where('email', '==', adminEmail);
+    docRef.get().then(function(querySnapshot) {
+        if (querySnapshot.docs.length) {
+            querySnapshot.forEach(function(doc) {
+                var data = doc.data();
+                document.getElementById('updateRoleUserId').value = doc.id;
+                document.getElementById('updateRoleUsername').value = data.username;
+                document.getElementById('updateRoleUserEmail').value = data.email;
+            })
+        } else {
+            document.getElementById('updateRole').classList.toggle('show');
+            alert("Corresponding User Not Found");
+            document.getElementById('make-email-admin').value = "";
+        }
+    });
+});
+
+//add admin cloud function
+const makeAdminBtn = document.getElementById('make-admin');
+makeAdminBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log("Initializing Admin Access to New Email");
+    const addAdminRole = functions.httpsCallable('addAdminRole');
+    var adminEmail = document.getElementById('updateRoleUserEmail').value;
+    addAdminRole({ email: adminEmail }).then(result => {
+        var date = addTime();
+        var id = document.getElementById('updateRoleUserId').value;
+        db.collection('Users').doc(id).set({ adminFrom: date }, { merge: true }).then(function() {
+            console.log("User Role Modified..");
+        });
+        console.log(result);
+        alert(adminEmail + " is admin now.");
+    })
+    document.getElementById('make-email-admin').value = "";
+    document.getElementById('updateRole').classList.toggle('show');
+});
+
+//Listen Click for Verify Member Button
+var verifyMember = document.querySelector('#verify-member');
+verifyMember.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.getElementById('updateRole').classList.toggle('show');
+    document.querySelector('#updateRole #purposeOfRole').innerText = "Verify Account Before Making Member";
+    var elements = document.getElementsByClassName('input member');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.display = "flex";
+    }
+    document.getElementById('make-admin').style.display = "none";
+    document.getElementById('make-member').style.display = "flex";
+
+    const memberEmail = document.getElementById('make-email-member').value;
+    var docRef = db.collection('Users').where('email', '==', memberEmail);
+    docRef.get().then(function(querySnapshot) {
+        if (querySnapshot.docs.length) {
+            querySnapshot.forEach(function(doc) {
+                var data = doc.data();
+                var userDatabaseRef = db.collection('Members').doc(doc.id);
+                userDatabaseRef.get().then(function(refDoc) {
+                    if (refDoc.exists) {
+                        document.getElementById('updateRole').classList.toggle('show');
+                        alert("User is already a Member");
+                    } else {
+                        document.getElementById('updateRoleUserId').value = doc.id;
+                        document.getElementById('updateRoleUsername').value = data.username;
+                        document.getElementById('updateRoleUserEmail').value = data.email;
+                    }
+                })
+            })
+        } else {
+            document.getElementById('updateRole').classList.toggle('show');
+            alert("No User Found in Database with the given email");
+            document.getElementById('make-email-member').value = "";
+        }
+    });
+})
+
+//add member cloud functions
+const makeMember = document.getElementById('make-member');
+makeMember.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log("Initializing Member Access to New Email");
+    const memberEmail = document.getElementById('make-email-member').value;
+    const addMembership = functions.httpsCallable('addMembership');
+    addMembership({ email: memberEmail }).then(result => {
+        console.log(result);
+        alert(memberEmail + " is member now.");
+        var docRef = db.collection('Users').where('email', '==', memberEmail);
+        docRef.get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                var data = doc.data();
+                var plan = document.getElementById('updateRoleUserMembershipPlan').value;
+                var planAmount = document.getElementById('updateRoleUserMembershipAmount').value;
+                var date = addTime();
+                lastDate = increaseDate(30);
+                db.collection('Members').doc(doc.id).set({ email: memberEmail, fullName: data.fullName, memberFrom: date, membershipExpire: lastDate, plan: plan, planAmount: planAmount }).then(function() {
+                    console.log("New Member data has been updated in database");
+                });
+            })
+        });
+    });
+    document.getElementById('make-email-member').value = "";
+    document.getElementById('updateRole').classList.toggle('show');
+});
+
+
+//Function to show Subscribers list at realtime from firestore
+var subscribersTable = document.querySelector('#subscribers-table');
+const subscribersList = (data) => {
+    let html = '';
+    let heading = `<tr class="table-heading">
+    <th scope="col" colspan="1">S.No</th>
+    <th scope="col" colspan="6">Email</th>
+    <th scope="col" colspan="2">Date</th>
+</tr>`;
+    var sNo = 1;
+    data.forEach(doc => {
+        const subscribers = doc.data();
+        console.log(subscribers);
+        const element = `<tr>
+        <td colspan="1">${sNo++}</td>
+        <td colspan="6">${subscribers.Email}</td>
+        <td colspan="2">${subscribers.created}</td>
+    </tr>`
+        html += element;
+    });
+    subscribersTable.innerHTML = heading + html;
+}
+
+function getSubscribers() {
+    console.log("fetching Subscribers List");
+    db.collection('Subscribers').onSnapshot(snapshot => {
+        subscribersList(snapshot.docs)
+    });
+}
+
+
+//Function to show Queries list at realtime from firestore
+var queriesTable = document.querySelector('#queries-table');
+const queriesList = (query) => {
+    let html = '';
+    let qheading = `<tr class="table-heading">
+    <th scope="col" colspan="1">S.No</th>
+    <th scope="col" colspan="3">Date</th>
+    <th scope="col" colspan="4">Name</th>
+    <th scope="col" colspan="6">Email</th>
+    <th scope="col" colspan="4">Mobile</th>
+    <th scope="col" colspan="6">Subject</th>
+    <th scope="col" colspan="6">Message</th>
+</tr>`;
+    var sNo = 1;
+    query.forEach(queryDoc => {
+        const queries = queryDoc.data();
+        console.log(queries);
+        const element = `<tr>
+        <td colspan="1">${sNo++}</td>
+        <td colspan="3">${queries.created}</td>
+        <td colspan="4">${queries.fullName}</td>
+        <td colspan="6">${queries.email}</td>
+        <td colspan="4">${queries.mobile}</td>
+        <td colspan="6">${queries.subject}</td>
+        <td colspan="6">${queries.message}</td>
+    </tr>`
+        html += element;
+    });
+    queriesTable.innerHTML = qheading + html;
+}
+
+function getQueries() {
+    console.log("fetching Queries List");
+    db.collection('Queries').onSnapshot(snapshot => {
+        queriesList(snapshot.docs)
+    });
+}
+
+
+//Function to display total Users
+function dashboardData() {
+    db.collection('Users').get().then((snapshot) => {
+        var totalUsers = document.getElementById('total-users-count');
+        totalUsers.innerText = snapshot.docs.length;
+    });
+    db.collection('Members').get().then((snapshot) => {
+        var totalMembers = document.getElementById('total-members-count');
+        var totalMembersAttendance = document.getElementById('total-members-count-attendance');
+        //console.log(snapshot.docs.length);
+        totalMembers.innerText = snapshot.docs.length;
+        totalMembersAttendance.innerText = snapshot.docs.length;
+    });
+    //function to check the new users today
+    var todayDate = addTime();
+    var newUsers = db.collection('Users').where('created', '==', todayDate);
+    newUsers.get().then(function(querySnapshot) {
+        var totalNewUsers = document.getElementById('total-new-users-count');
+        console.log("Total New Users : " + querySnapshot.docs.length);
+        totalNewUsers.innerText = querySnapshot.docs.length;
+    });
+
+    var expireDate = db.collection('Members').where('membershipExpire', '<', todayDate);
+    expireDate.get().then(function(snapshot) {
+        console.log("Expired Membership : " + snapshot.docs.length);
+        document.getElementById('expired-membership-count').innerText = snapshot.docs.length;
+    });
+
+    //query for total present and absent memebers attendance from database
+    var attendance = db.collection('Attendance').doc(todayDate);
+    attendance.get().then(function(doc) {
+        var data = doc.data();
+        document.getElementById('total-present-attendance').innerText = data.Present;
+        document.getElementById('total-absent-attendance').innerText = data.Absent;
+    })
+}
+
+
+
+//function to retrieve admin-user data
+//first disable all the user input fields and enable them on listing click on edit button
+var username = document.querySelector('#username');
+var fullName = document.querySelector('#fullName');
+var email = document.querySelector('#email');
+var photoURL = document.querySelector('#photoURL');
+var mobile = document.querySelector('#mobile');
+var dob = document.querySelector('#dob');
+var locality = document.querySelector('#locality');
+var address1 = document.querySelector('#address1');
+var city = document.querySelector('#city');
+var state = document.querySelector('#state');
+var country = document.querySelector('#country');
+$(".form .input>:input").prop('readonly', true);
+Auth.onAuthStateChanged(function(User) {
+    if (User) {
+        dashboardData();
+        getQueries();
+        getSubscribers();
+        var userDatabaseRef = db.collection('Users').doc(User.uid);
+        userDatabaseRef.get().then(function(doc) {
+            if (doc.exists) {
+                console.log("Document opened with id : " + doc.id);
+                var user = doc.data();
+                username.value = user.username;
+                fullName.value = user.fullName;
+                email.value = user.email;
+                photoURL.value = user.photoURL;
+                mobile.value = parseInt(user.mobile);
+                dob.value = user.dob;
+                locality.value = user.locality;
+                address1.value = user.address1;
+                city.value = user.city;
+                state.value = user.state;
+                country.value = user.country;
+            } else {
+                console.log("User Information Doesnt Exists.")
+            }
+        });
+        const editUser = document.getElementById('edit-user-information');
+        editUser.addEventListener('click', (event) => {
+            event.preventDefault();
+            $(".form .input>:input").prop('readonly', false);
+            $(".form-content .form-username-area>:input").prop('readonly', true);
+        })
+        const saveUser = document.getElementById('save-user-information');
+        saveUser.addEventListener('click', (event) => {
+            event.preventDefault();
+            userDatabaseRef.set({
+                fullName: fullName.value,
+                email: email.value,
+                photoURL: photoURL.value,
+                mobile: mobile.value,
+                dob: dob.value,
+                locality: locality.value,
+                address1: address1.value,
+                city: city.value,
+                state: state.value,
+                country: country.value
+            }, { merge: true }).then(function() {
+                $(".form .input>:input").prop('readonly', true);
+                console.log("User data saved Successfully.");
+            });
+        });
+    } else console.log("Login to Load Dashboard Components");
+});
--- javascript/index.js
+++ javascript/index.js
@@ -1,65 +1,134 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
+const firebaseConfig = {
+    apiKey: "AIzaSyAJBdrB9l7aCBJ6tbqe19xhFaT_hr0rnh4",
+    authDomain: "fitness-edge-212.firebaseapp.com",
+    databaseURL: "https://fitness-edge-212.firebaseio.com",
+    projectId: "fitness-edge-212",
+    storageBucket: "fitness-edge-212.appspot.com",
+    messagingSenderId: "412671339273",
+    appId: "1:412671339273:web:d3ee36a1a0d39902ab01d8",
+    measurementId: "G-GXVYHKVCE0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const Auth = firebase.auth();
+const db = firebase.firestore();
+const functions = firebase.functions();
 
-    document.getElementById("user_div").style.display = "flex";
-    document.getElementById("login_div").style.display = "none";
-    document.getElementById("signup_div").style.display = "none";
-
-    var user = firebase.auth().currentUser;
-
-    if(user != null){
-
-      var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-
-    }
-
-  } else {
-    // No user is signed in.
-
-    document.getElementById("user_div").style.display = "none";
-    document.getElementById("login_div").style.display = "flex";
-    document.getElementById("signup_div").style.display = "none";
-
-  }
+const newRegistration = document.getElementById('registerNewUser-btn');
+newRegistration.addEventListener('click', (event) => {
+    var email = document.getElementById('signup-email').value;
+    var password = document.getElementById('signup-password').value;
+    Auth.createUserWithEmailAndPassword(email, password).then(function() {
+        console.log("User account Created : " + Auth.currentUser.email);
+        Auth.currentUser.sendEmailVerification();
+        var username = document.getElementById('signup-username').value;
+        Auth.currentUser.updateProfile({ displayName: username });
+        var username = document.getElementById('signup-username').value;
+        db.collection("Usernames").doc(Auth.currentUser.uid).set({ username: username }).then(function(docRef) {
+            console.log("Document written with ID: " + docRef.id);
+        }).catch(function(error) {
+            console.error("Error adding document: " + error.code);
+        });
+        var created = addTime();
+        db.collection("Users").doc(Auth.currentUser.uid).set({
+            username: username,
+            created: created,
+            fullName: '',
+            email: email,
+            password: password,
+            photoURL: '',
+            mobile: '',
+            dob: '',
+            locality: '',
+            address1: '',
+            city: '',
+            state: '',
+            country: ''
+        }, { merge: true });
+        var form = document.getElementsByClassName('cd-signin-modal');
+        form[0].classList.remove('cd-signin-modal--is-visible');
+    }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage)
+    });
 });
 
-function login(){
+function login() {
+    var userEmail = document.getElementById("signin-email").value;
+    var userPass = document.getElementById("signin-password").value;
+    Auth.signInWithEmailAndPassword(userEmail, userPass).then(function() {
+        console.log(Auth.currentUser.email + " logged in successfully.");
+        var form = document.getElementsByClassName('cd-signin-modal');
+        form[0].classList.remove('cd-signin-modal--is-visible');
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == "auth/user-not-found") {
+            alert("User is not registered with us.");
+        } else if (errorCode == "auth/wrong-password") {
+            alert("Wrong Email/Password entered. Try Again!!");
+        }
 
-  var userEmail = document.getElementById("email").value;
-  var userPass = document.getElementById("password").value;
-
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-
-    // ...
-  });
+    });
 
 }
 
-function signup(){
-  document.getElementById("user_div").style.display = "none";
-  document.getElementById("login_div").style.display = "none";
-  document.getElementById("signup_div").style.display = "flex";
+function logout() {
+    Auth.signOut().then(function() {
+        // Sign-out successful.
+        console.log("logged out successfully.");
+    }).catch(function(error) {
+        // An error happened.
+        console.log("Logout Error" + error.message);
+    });
+}
+//this function get datestamp for the databasce created
+function addTime() {
+    var timestamp = firebase.firestore.Timestamp.now();
+    var temp = timestamp.toDate() + "";
+    var created = temp.slice(0, 15);
+    return created;
 }
 
-function create_account(){
-  var userEmail = document.getElementById("signup-email").value;
-  var userPass = document.getElementById("signup-password").value;
-
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+function increaseDate(day) {
+    var timestamp = firebase.firestore.Timestamp.now().toDate();
+    timestamp.setDate(timestamp.getDate() + day);
+    var temp = timestamp + "";
+    var created = temp.slice(0, 15);
+    return created;
 }
 
-function logout(){
-  firebase.auth().signOut();
-}
+//function to  fetch email of subscribers from the website.
+const subscribe = document.getElementById('subscribe-btn');
+subscribe.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = document.getElementById("subscriber-email").value;
+    if (email) {
+        var created = addTime();
+        db.collection('Subscribers').add({ Email: email, created: created }).then(function(doc) {
+            console.log(doc.id);
+        }).catch(function(error) {
+            console.log("Error : " + error.code)
+        });
+    }
+})
+
+//function to fetch Queries from the website
+const submitQuery = document.getElementById('submit-query');
+submitQuery.addEventListener('click', (event) => {
+    event.preventDefault();
+    const fullName = document.getElementById('fname').value;
+    const email = document.getElementById('email').value;
+    const mobile = document.getElementById('mobile').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+    var created = addTime();
+    db.collection('Queries').add({ fullName: fullName, email: email, mobile: mobile, subject: subject, message: message, created: created }).then(function(doc) {
+        console.log("Document created at " + doc.id);
+    }).catch(function(error) {
+        console.log("Error : " + error.code)
+    });
+})
